80b1fc52e574ea5f30ce5a789ea33c248f60ab9e
Anna <46578108+anchsk@users.noreply.github.com>
2025-03-11 22:32:22 +0100

    add pizza

---
+++ pizza.py
@@ -0,0 +1,9 @@
+pizzas = ['Marinara', 'Romana', 'Napoletana']
+
+for pizza in pizzas:
+    print(pizza)
+    print() # new line or print(pizza + '\n')
+print("I like pizza " + pizza) 
+
+# pizzas.sort(reverse=True)
+print(sorted(pizzas))
