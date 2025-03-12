08b019952301634d42d003c4d552e2f26ed51a8b
Anna <46578108+anchsk@users.noreply.github.com>
2025-03-12 12:01:15 +0100

    list comprehension with enumerate and if

--- list.py
+++ list.py
@@ -5,8 +5,27 @@ print(numbers)
 even_numbers = list(range(2,11,2))
 print(even_numbers)
 
-print(min(numbers))
+""" print(min(numbers))
 print(max(numbers))
-print(sum(numbers))
+print(sum(numbers)) """
 
-hello = ''
+
+def skip_elements(elements):
+	# code goes here
+	result = []
+	for i, el in enumerate(elements):
+		if i%2 == 0:
+			result.append(el)
+
+	
+	return result
+letters = ["a", "b", "c", "d", "e", "f", "g"]
+print(skip_elements(letters)) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+def skip_elements_2(elements):
+    # result = []
+    result = [x for (i,x) in enumerate(elements) if i%2 == 0]
+    return result
+
+print(skip_elements_2(letters))
