ee2d3c54f8c3f7c4f3f9d36b6b8bca594230da10
Anna <46578108+anchsk@users.noreply.github.com>
2025-03-12 11:53:02 +0100

    list example

---
+++ list.py
@@ -0,0 +1,12 @@
+numbers = list(range(1,6))
+print(numbers)
+# [1, 2, 3, 4, 5]
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+hello = ''
