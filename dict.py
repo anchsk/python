176d53f57c8c1d8b030069cbe750e55c86665f96
Anna <46578108+anchsk@users.noreply.github.com>
2025-03-13 12:05:04 +0100

    add dict

---
+++ dict.py
@@ -0,0 +1,36 @@
+my_dict = {'a': 10, 'b': 20, 'c': 30}
+for item in my_dict:
+    print(item)
+# a b c
+for item in my_dict:
+    print(my_dict[item])
+# 10 20 30
+
+for (item, value) in my_dict.items(): # <<< items!
+    print("{} is {}".format(item,value))
+
+# a is 10
+# b is 20
+# c is 30
+
+print(my_dict.keys())
+# dict_keys(['a', 'b', 'c'])
+print(my_dict.values())
+# dict_values([10, 20, 30])
+
+
+print(my_dict)
+# {'a': 10, 'b': 20, 'c': 30}
+print(my_dict.items())
+# dict_items([(a,10), ('b', 20), ('c', 30)])
+a = tuple(my_dict.items())
+print(a)
+# (('a', 10), ('b', 20), ('c', 30))
+b = list(my_dict.items())
+print(b)
+# [('a', 10), ('b', 20), ('c', 30)]
+c = str(my_dict.items())
+print(c)
+# dict_items([('a', 10), ('b', 20), ('c', 30)])
+print(type(c)) 
+# <class 'str'>
