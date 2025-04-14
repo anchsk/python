7e1af155757560b46cf5ad1386d0052a3d4324ff
Anna <46578108+anchsk@users.noreply.github.com>
2025-04-14 12:30:59 +0200

    flask isbn book search api

---
+++ isbn/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello"
+
+
+@app.route("/book/<isbn>")
+def get_author(isbn):
+    try:
+        res = requests.get(f"https://openlibrary.org/isbn/{isbn}.json")
+        if res.status_code == 200:
+            return {"message": res.json()}
+        elif res.status_code == 404:
+            return {"message": "Something went wrong"}, 404
+    except requests.exceptions.RequestException as e:
+        # Handle request exceptions
+        return {"error": str(e)}, 500
