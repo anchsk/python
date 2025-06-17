8ba0549e11bd408ecbbb4ef589dfae28a5b0f566
Anna <46578108+anchsk@users.noreply.github.com>
2025-06-17 11:06:27 +0200

    list comprehension

--- comprehension.py
+++ comprehension.py
@@ -56,3 +56,5 @@ print(type(gen_obj))
 for items in gen_obj:
     print(items, end = " ")
     
+    
+my_employees = [{'id': 12345, 'name': 'John', 'department': 'Kitchen'}, {'id': 12456, 'name': 'Paul', 'department': 'House Floor'}, {'id': 12478, 'name': 'Sarah', 'department': 'Management'}, {'id': 12434, 'name': 'Lisa', 'department': 'Cold Storage'}, {'id': 12483, 'name': 'Ryan', 'department': 'Inventory Mgmt'}, {'id': 12419, 'name': 'Gill', 'department': 'Cashier'}]
